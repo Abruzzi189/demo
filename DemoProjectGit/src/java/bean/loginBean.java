f2ca8841749f8f1fb351c55880140e9509d5ac28
ThoanVV <vuvithoan189@gmail.com>
2018-09-08 16:57:48 +0700

    hihi

--- DemoProjectGit/src/java/bean/loginBean.java
+++ DemoProjectGit/src/java/bean/loginBean.java
@@ -6,18 +6,42 @@
 package bean;
 
 import entity.User;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Admin
  */
+@ManagedBean
+@SessionScoped
 public class loginBean {
-    private User user;
-    
-    public void checkLogin()
+
+    private User user;    
+
+    @PostConstruct
+    public void init()
     {
+        user = new User();
+    }
+    public void checkLogin() {
+        String url = "";
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("www.w3school.com");
+        } catch (IOException ex) {
+            Logger.getLogger("checkLogin").log(Level.SEVERE, null, ex);
+        }
         
     }
+
     public User getUser() {
         return user;
     }
@@ -25,6 +49,6 @@ public class loginBean {
     public void setUser(User user) {
         this.user = user;
     }
-    
-    
+  
+
 }
